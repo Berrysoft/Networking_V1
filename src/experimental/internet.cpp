4505759284910028d9fad17180304937e16de8f6
王宇逸 <Strawberry_Str@hotmail.com>
2019-02-10 17:56:15 +0800

    Switch to Windows.

--- src/experimental/internet.cpp
+++ src/experimental/internet.cpp
@@ -1,5 +1,4 @@
 #include <experimental/internet>
-#include <net/if.h>
 
 namespace std::experimental::net
 {
@@ -10,7 +9,7 @@ namespace ip
 address_v4 make_address_v4(const char* str, error_code& ec) noexcept
 {
     address_v4::bytes_type bytes;
-    int r{ ::inet_pton(AF_INET, str, bytes.begin()) };
+    int r{ ::inet_pton(AF_INET, str, bytes.data()) };
     if (r != 1)
         ec = make_error_code(errc::invalid_argument);
     return address_v4{ bytes };
@@ -18,29 +17,12 @@ address_v4 make_address_v4(const char* str, error_code& ec) noexcept
 
 address_v6 make_address_v6(const char* str, error_code& ec) noexcept
 {
-    const char* if_name = strchr(str, '%');
-    char src_buf[257];
-    const char* src_ptr = str;
-    if (if_name - str > 256)
-    {
+    sockaddr_in6 s{};
+    int size = sizeof(sockaddr_in6);
+    int r{ ::WSAStringToAddressA(const_cast<char*>(str), 6, nullptr, (LPSOCKADDR)&s, &size) };
+    if (r != 0)
         ec = make_error_code(errc::invalid_argument);
-        return {};
-    }
-    memcpy(src_buf, str, if_name - str);
-    src_buf[if_name - str] = 0;
-    src_ptr = src_buf;
-    address_v6::bytes_type bytes;
-    int r{ ::inet_pton(AF_INET6, str, bytes.begin()) };
-    if (r != 1)
-        ec = make_error_code(errc::invalid_argument);
-    int scope_id{ 0 };
-    bool is_link_local = (bytes[0] == 0xfe) && ((bytes[1] & 0xc0) == 0x80);
-    bool is_multicast_link_local = (bytes[0] == 0xff) && ((bytes[1] & 0x0f) == 0x02);
-    if (is_link_local || is_multicast_link_local)
-        scope_id = ::if_nametoindex(if_name + 1);
-    if (!scope_id)
-        scope_id = atoi(if_name + 1);
-    return address_v6{ bytes, scope_id };
+    return address_v6{ s.sin6_addr, s.sin6_scope_id };
 }
 
 address make_address(const char* str, error_code& ec) noexcept
@@ -73,12 +55,12 @@ basic_address_iterator<address_v6>& basic_address_iterator<address_v6>::operator
 {
     for (int i = 15; i >= 0; --i)
     {
-        if (address_.addr_.s6_addr[i] < 0xFF)
+        if (address_._Addr().s6_addr[i] < 0xFF)
         {
-            ++address_.addr_.s6_addr[i];
+            ++address_._Addr().s6_addr[i];
             break;
         }
-        address_.addr_.s6_addr[i] = 0;
+        address_._Addr().s6_addr[i] = 0;
     }
     return *this;
 }
@@ -86,12 +68,12 @@ basic_address_iterator<address_v6>& basic_address_iterator<address_v6>::operator
 {
     for (int i = 15; i >= 0; --i)
     {
-        if (address_.addr_.s6_addr[i] > 0)
+        if (address_._Addr().s6_addr[i] > 0)
         {
-            --address_.addr_.s6_addr[i];
+            --address_._Addr().s6_addr[i];
             break;
         }
-        address_.addr_.s6_addr[i] = 0xFF;
+        address_._Addr().s6_addr[i] = 0xFF;
     }
     return *this;
 }
